d1322b4d6d4f7cdc435584a07ef8bae4017cb9d7
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-08-05 10:02:29 -0400

    Update .cmd file to launch from any directory

--- teraterm-connections.cmd
+++ teraterm-connections.cmd
@@ -1,2 +1,6 @@
-python3 teraterm_macro.py -f teraterm_connections.csv -p C:\util\teraterm-4.106 -l C:\util\teraterm-4.106\logs
+rem python3 %cd%\teraterm_macro.py -f %cd%\teraterm_targets.csv -p %cd% -l %cd%\logs
+rem python3 teraterm_macro.py -f teraterm_connections.csv -p C:\util\teraterm-4.106 -l C:\util\teraterm-4.106\logs
+
+rem python3 C:\util\teraterm-4.106\macros\teraterm_macro.py -f C:\util\teraterm-4.106\macros\teraterm_targets.csv -p C:\util\teraterm-4.106 -l C:\util\teraterm-4.106\logs
+python3 %cd%\macros\teraterm_macro.py -f %cd%\macros\teraterm_targets.csv -p %cd% -l %cd%\logs
 pause

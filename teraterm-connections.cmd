d10da23ece6fc10bd634076f84cca86cfdb80b53
William Blair <60581672+williamblair333@users.noreply.github.com>
2023-11-18 01:37:55 -0500

    Add files via upload

--- teraterm-connections.cmd
+++ teraterm-connections.cmd
@@ -1,1 +1,3 @@
-python3 %cd%\macros\teraterm_macro.py -f %cd%\macros\teraterm_targets.csv -p %cd% -l %cd%\logs
+rem python3 "%cd%\macros\teraterm_macro.py" -f "%cd%\macros\teraterm_targets.csv" -p "%cd%" -l "%cd%\logs"
+python "%cd%\macros\teraterm_macro.py" -f "%cd%\macros\teraterm_targets.csv" -p "%cd%" -l "%cd%\logs"
+pause
